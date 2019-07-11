4d0673b562ece0f1cc39f761773d212e71675e95
MariaZafar <maria.zafar9715@gmail.com>
2019-07-11 10:57:14 +0500

    5th commit

--- screens/Main.js
+++ screens/Main.js
@@ -5,16 +5,82 @@ import { Card, Badge, Button, Block, Text } from '../components';
 import { theme, mocks } from '../constants';
 import firebase from 'firebase';
 const { width } = Dimensions.get('window');
+import { Permissions, Notifications } from 'expo';
+
+const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
 
 class Main extends Component {
+
+  registerForPushNotificationsAsync = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+  
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+  
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+  
+    try {
+      // Get the token that uniquely identifies this device
+      let token = await Notifications.getExpoPushTokenAsync();
+  
+      // POST the token to your backend server from where you can retrieve it to send push notifications.
+       console.log(token)
+  
+       const dbh = firebase.firestore();
+  
+          dbh.collection("users").doc(""+this.currentUser.uid).update({
+            pushtoken :token
+          })
+      //   .firestore()
+      //   .ref('users/' + this.currentUser.uid + '/push_token')
+      //   .set(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  sendPushNotification = () => {
+    let response = fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: 'ExponentPushToken[ubbWTmKWiwr6Z_IIqSsEbW]',
+        sound: 'default',
+        title: 'Demo',
+        body: 'Demo notificaiton'
+      })
+    });
+  };
+ 
+
   state = {
     active: 'Products',
     categories: [],
   }
 
-  componentDidMount() {
-    this.setState({ categories: this.props.categories });
+  async componentDidMount() {
+    this.currentUser = await firebase.auth().currentUser;
+    await this.registerForPushNotificationsAsync();
   }
+
+
+  // componentDidMount() {
+  //   this.setState({ categories: this.props.categories });
+  // }
 
   handleTab = tab => {
     const { categories } = this.props;
